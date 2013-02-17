ea965c4b94f7b63f811673d105d162ec5d49dfb8
Takacs, David <david.takacs@prezi.com>
2013-02-17 16:12:59 +0100

    Serialization and code generation happens at the right place.

--- utg.py
+++ utg.py
@@ -120,23 +120,27 @@ def capture(func):
         return ret
     return func2
 
+# TODO CallHistory class
 call_info = { }
 def append_call_info(id, funcname, args, kwargs, ret):
     global call_info
     key = funcname
     if not key in call_info:
         call_info[key] = []
-    call_info[key].append((id, funcname, Repo.marshal().freeze(args), Repo.marshal().freeze(kwargs), Repo.marshal().freeze(ret)))
+    call_info[key].append((id, funcname, args, kwargs, ret))
 
 def mock_code():
     global call_info
     code = ""
     for key in call_info:
         funcname = key
-        code += "  def mock_"+funcname+"(*args, **kwargs):\n"
-        for (id, funcname, args, kwargs, ret) in call_info[key]:
-            code += "    if args == "+args+" and kwargs == "+kwargs+":\n"
-            code += "      return "+ret+"\n\n"
+        code += "  def mock_" + funcname + "(*args, **kwargs):\n"
+        for (id, funcname, s_args, s_kwargs, s_ret) in call_info[key]:
+            c_args = Repo.marshal().unserialize_code(s_args)
+            c_kwargs = Repo.marshal().unserialize_code(s_kwargs)
+            c_ret = Repo.marshal().unserialize_code(s_ret)
+            code += "    if args == " + c_args + " and kwargs == " + c_kwargs + ":\n"
+            code += "      return " + c_ret + "\n\n"
     return code
 
 def test_code():
@@ -145,29 +149,36 @@ def test_code():
             "from ent import * \n\n" + \
             "class TestEnt(unittest.TestCase): \n\n"
     for key in call_info:
-        for (id, funcname, args, kwargs, ret) in call_info[key]:
+        for (id, funcname, s_args, s_kwargs, s_ret) in call_info[key]:
+            c_args = Repo.marshal().unserialize_code(s_args)
+            c_kwargs = Repo.marshal().unserialize_code(s_kwargs)
+            c_ret = Repo.marshal().unserialize_code(s_ret)
             code += "  def test_" + key + "_" + str(id) + "(self):\n"
-            code += "    actual = " + funcname + "(*" + args + ", **" + kwargs + ")\n"
-            code += "    expected = " + ret + "\n"
+            code += "    actual = " + funcname + "(*" + c_args + ", **" + c_kwargs + ")\n"
+            code += "    expected = " + c_ret + "\n"
             code += "    self.assertEqual(expected, actual)\n\n"
     code += "if __name__ == '__main__': \n" + \
             "  unittest.main() \n"
     return code
 
 def call_enter(id, funcname, args, kwargs):
-    log_call_enter(id, funcname, args, kwargs)
+    s_args = Repo.marshal().serialize(args)
+    s_kwargs = Repo.marshal().serialize(kwargs)
+    log_call_enter(id, funcname, s_args, s_kwargs)
+    # TODO Reachability.updatePath()
     for (depth, item) in Repo.stack().items():
         Repo.reachability().update(item[1], funcname, depth)
-    Repo.stack().push((id, funcname, args, kwargs))
+    Repo.stack().push((id, funcname, s_args, s_kwargs, ))
 
-def log_call_enter(id, funcname, args, kwargs):
-    print get_indent() + "CALL ", funcname, "(*", Repo.marshal().serialize(args), ", **", Repo.marshal().serialize(kwargs), ")"
+def log_call_enter(id, funcname, s_args, s_kwargs):
+    print get_indent() + "CALL ", funcname, "(*", s_args, ", **", s_kwargs, ")"
 
 def call_return(id, ret):
-    (popid, funcname, args, kwargs) = Repo.stack().popWhile(lambda item: item[0] != id)
-    log_call_return(id, ret)
-    append_call_info(id, funcname, args, kwargs, ret)
+    (popid, funcname, s_args, s_kwargs) = Repo.stack().popWhile(lambda item: item[0] != id)
+    s_ret = Repo.marshal().serialize(ret)
+    log_call_return(id, s_ret)
+    append_call_info(id, funcname, s_args, s_kwargs, s_ret)
 
-def log_call_return(id, ret):
-    print get_indent() + "RETURN ", Repo.marshal().serialize(ret)
+def log_call_return(id, s_ret):
+    print get_indent() + "RETURN ", s_ret
 
