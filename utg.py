22d8f4b62adfc50430c427054f7aa1a93d0e5fa6
Takacs, David <david.takacs@prezi.com>
2013-02-17 14:27:48 +0100

    Extracted Stack, and minors.

--- utg.py
+++ utg.py
@@ -11,7 +11,6 @@ def test_mode():
     runmode = "test"
 
 next_id = 1
-stack = []
 indent_unit = "    "
 reachability = {}
 
@@ -32,15 +31,48 @@ class ReprMarshal (AbstractMarshal):
     def unserialize_code(self, serialized):
         return serialized
 
+class Stack:
+    def __init__(self):
+        self._list = []
+
+    def push(self, obj):
+        self._list.append(obj)
+
+    def pop(self):
+        return self._list.pop()
+
+    def popWhile(self, callback):
+        item = self.pop()
+        while self.len() > 0 and callback(item):
+            item = self.pop()
+        if callback(item):
+            raise Error("Stack item not found")
+        return item
+
+    def len(self):
+        return len(self._list)
+
+    def items(self):
+        i = self.len()
+        for item in self._list:
+            yield (i, item)
+            i -= 1
+
 class Repo:
 
     _marshal = None
-
     @staticmethod
     def marshal():
         if not isinstance(Repo._marshal, AbstractMarshal):
             Repo._marshal = ReprMarshal()
         return Repo._marshal;
+
+    _stack = None
+    @staticmethod
+    def stack():
+        if not isinstance(Repo._stack, Stack):
+            Repo._stack = Stack()
+        return Repo._stack;
 
 def get_next_id():
     global next_id
@@ -49,8 +81,8 @@ def get_next_id():
     return id
 
 def get_indent():
-    global stack, indent_unit
-    return "".join([ indent_unit for i in range(len(stack))])
+    global indent_unit
+    return "".join([ indent_unit for i in range(Repo.stack().len())])
 
 def capture(func):
     """ Decorator which captures the args and the return values of the function. """
@@ -61,12 +93,13 @@ def capture(func):
         id = get_next_id()
         call_enter(id, func.__name__, args, kwargs)
         ret = func(*args, **kwargs)
+        # TODO try-except
         call_return(id, ret)
         return ret
     return func2
 
 mocks = { }
-def append_mocks(funcname, args, kwargs, ret):
+def append_mocks(id, funcname, args, kwargs, ret):
     global mocks
     key = funcname
     if not key in mocks:
@@ -92,6 +125,10 @@ def append_test(funcname, id, code):
         tests[key] = []
     tests[key].append(code)
 
+def append_tests(id, funcname, args, kwargs, ret):
+    append_test(funcname, id, "    actual = " + funcname + "(*" + Repo.marshal().freeze(args) + ", **" + Repo.marshal().freeze(kwargs) + ")")
+    append_test(funcname, id, "    expected = " + Repo.marshal().freeze(ret))
+
 def test_code():
     global tests
     code =  "import unittest \n" + \
@@ -105,36 +142,30 @@ def test_code():
             "  unittest.main() \n"
     return code
 
-def call_enter(id, funcname, args, kwargs):
-    log_call_enter(id, funcname, args, kwargs)
-    global stack
+def update_reachability(id, funcname, args, kwargs):
     global reachability
-    depth = len(stack)
-    for (prev_id, prev_funcname, prev_args, prev_kwargs) in stack:
+    for (depth, (prev_id, prev_funcname, prev_args, prev_kwargs)) in Repo.stack().items():
         if not prev_funcname in reachability:
             reachability[prev_funcname] = {}
         if not funcname in reachability[prev_funcname]:
             reachability[prev_funcname][funcname] = depth;
         else:
             reachability[prev_funcname][funcname] = min(depth, reachability[prev_funcname][funcname])
-    stack.append((id, funcname, args, kwargs))
+
+def call_enter(id, funcname, args, kwargs):
+    log_call_enter(id, funcname, args, kwargs)
+    update_reachability(id, funcname, args, kwargs)
+    Repo.stack().push((id, funcname, args, kwargs))
 
 def log_call_enter(id, funcname, args, kwargs):
     print get_indent() + "CALL ", funcname, "(*", Repo.marshal().serialize(args), ", **", Repo.marshal().serialize(kwargs), ")"
 
 def call_return(id, ret):
-    global stack
-    (popid, funcname, args, kwargs) = stack.pop()
-    while len(stack) > 0 and popid != id:
-        print "WARNING, a return is lost"
-        (popid, funcname, args, kwargs) = stack.pop()
-    if popid != id:
-        raise Error("Call #" + str(id) + " not found")
+    (popid, funcname, args, kwargs) = Repo.stack().popWhile(lambda item: item[0] != id)
     log_call_return(id, ret)
-    append_test(funcname, id, "    actual = " + funcname + "(*" + Repo.marshal().freeze(args) + ", **" + Repo.marshal().freeze(kwargs) + ")")
-    append_test(funcname, id, "    expected = " + Repo.marshal().freeze(ret))
-    append_mocks(funcname, args, kwargs, ret)
+    append_tests(id, funcname, args, kwargs, ret)
+    append_mocks(id, funcname, args, kwargs, ret)
 
 def log_call_return(id, ret):
-    print get_indent() + "RETURN ", Repo.marshal().freeze(ret)
+    print get_indent() + "RETURN ", Repo.marshal().serialize(ret)
 
