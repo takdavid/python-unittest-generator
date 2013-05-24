5277579318afe4992195698ad0103542049ec4d9
Takacs, David <david.takacs@prezi.com>
2013-05-24 11:04:07 +0200

    Renamed id to the more meaningful tick.

--- utg.py
+++ utg.py
@@ -41,7 +41,7 @@ def capture(function):
     def wrapper(*args, **kwargs):
         serialize = Repo.marshal().serialize
         callhistory = Repo.callhistory()
-        callid = callhistory.get_next_id()
+        callid = callhistory.get_tick()
         if is_instance_method(function):
             key = function.im_self.__module__ + "." + function.im_self.__class__.__name__ + "." + function.__name__
             callhistory.call_object(callid, id(function.im_self))
@@ -185,7 +185,7 @@ class Reachability:
 class CallHistoryBuilder(object):
 
     def __init__(self):
-        self.next_id = 0
+        self.tick = 0
         self.calls = {}
         self.results = {}
         self.linear = []
@@ -195,21 +195,21 @@ class CallHistoryBuilder(object):
         self.object_calls = {}
 
     def replay(self, that):
-        for (indent, what, id) in self.linear:
+        for (indent, what, tick) in self.linear:
             that.indent = indent
             if what == 'enter':
-                if self.object_calls.get(id):
-                    that.call_object(id, self.object_calls[id])
-                that.call_enter(id, * self.calls[id])
+                if self.object_calls.get(tick):
+                    that.call_object(tick, self.object_calls[tick])
+                that.call_enter(tick, * self.calls[tick])
             elif what == 'result':
-                that.call_result(id, * self.results[id])
-        for id in self.directive:
-            that.directive[id] = self.directive[id]
+                that.call_result(tick, * self.results[tick])
+        for tick in self.directive:
+            that.directive[tick] = self.directive[tick]
 
-    def get_next_id(self):
-        id = self.next_id
-        self.next_id += 1
-        return id
+    def get_tick(self):
+        tick = self.tick
+        self.tick += 1
+        return tick
 
     def get_indent(self):
         return self.indent
@@ -217,19 +217,19 @@ class CallHistoryBuilder(object):
     def call_object(self, callid, objid):
         self.object_calls[callid] = objid
 
-    def call_enter(self, id, key, s_args, s_kwargs):
-        self.calls[id] = [key, s_args, s_kwargs]
-        self.linear.append((self.get_indent(), 'enter', id, ))
+    def call_enter(self, tick, key, s_args, s_kwargs):
+        self.calls[tick] = [key, s_args, s_kwargs]
+        self.linear.append((self.get_indent(), 'enter', tick, ))
 
-    def call_result(self, id, s_res, s_exc):
-        self.results[id] = [s_res, s_exc]
-        self.linear.append((self.get_indent(), 'result', id, ))
+    def call_result(self, tick, s_res, s_exc):
+        self.results[tick] = [s_res, s_exc]
+        self.linear.append((self.get_indent(), 'result', tick, ))
 
-    def isTestable(self, id):
-        return id not in self.directive or "TEST" in self.directive[id] or not "SKIP" in self.directive[id]
+    def isTestable(self, tick):
+        return tick not in self.directive or "TEST" in self.directive[tick] or not "SKIP" in self.directive[tick]
 
-    def isMockable(self, id):
-        return id in self.directive and "MOCK" in self.directive[id]
+    def isMockable(self, tick):
+        return tick in self.directive and "MOCK" in self.directive[tick]
 
 class CallHistoryWriter(CallHistoryBuilder):
 
@@ -239,16 +239,16 @@ class CallHistoryWriter(CallHistoryBuilder):
     def write(self, str):
         self.log.append(str)
 
-    def call_enter(self, id, key, s_args, s_kwargs):
-        if self.object_calls.get(id):
-            objidpart = "$" + str(self.object_calls[id]) + "$"
+    def call_enter(self, tick, key, s_args, s_kwargs):
+        if self.object_calls.get(tick):
+            objidpart = "$" + str(self.object_calls[tick]) + "$"
         else:
             objidpart = ""
         self.write(self.get_indent() + "CALL " + key + " " + objidpart)
         self.write(self.get_indent() + "ARGS " + s_args)
         self.write(self.get_indent() + "KWARGS " + s_kwargs)
 
-    def call_result(self, id, s_res, s_exc):
+    def call_result(self, tick, s_res, s_exc):
         if s_exc == "None":
             self.write(self.get_indent() + "RETURN " + s_res)
         else:
@@ -298,17 +298,17 @@ class CallHistoryParser(CallHistoryBuilder):
 
     def parse(self, log):
         """ parse annotated call history """
-        id_for_indent = {}
+        tick_for_indent = {}
         lines = iter(log)
         for line in lines:
             m = re.match("^(\s*)((?:SKIP|TEST)?) *?CALL (.*?) ((\$(.*)\$)?)\s*$", line)
             if m:
                 (indent, direct, key, objidpart) = (m.group(1), m.group(2), m.group(3), m.group(6))
                 objid = int(objidpart) if objidpart else None
-                id = id_for_indent[indent] = self.get_next_id()
+                tick = tick_for_indent[indent] = self.get_tick()
                 self.setDirective(indent, key, direct)
-                self.directive[id] = self.getDirectives(indent, key)
-                call = [ id, key, None, None ]
+                self.directive[tick] = self.getDirectives(indent, key)
+                call = [ tick, key, None, None ]
                 line = lines.next()
                 m = re.match("^(\s*)ARGS (.*?)\s*$", line)
                 if m:
@@ -319,7 +319,7 @@ class CallHistoryParser(CallHistoryBuilder):
                     call[3] = m.group(2)
                 self.indent = indent
                 if objid:
-                    self.call_object(id, objid)
+                    self.call_object(tick, objid)
                 self.call_enter(*call)
                 continue
             m = re.match("^(\s*)(MOCK|SKIP|TEST) (.*?)\s*$", line)
@@ -331,12 +331,12 @@ class CallHistoryParser(CallHistoryBuilder):
             if m:
                 indent = m.group(1)
                 self.indent = indent
-                id = id_for_indent[indent]
+                tick = tick_for_indent[indent]
                 if m.group(2) == "RETURN":
-                    self.call_result(id, m.group(3), None)
+                    self.call_result(tick, m.group(3), None)
                 elif m.group(2) == "RAISE":
-                    self.call_result(id, None, m.group(3))
-                id_for_indent[indent] = None
+                    self.call_result(tick, None, m.group(3))
+                tick_for_indent[indent] = None
                 self.invalidateDirectives(indent)
                 continue
             raise Exception("ERROR INVALID LINE " + line)
@@ -352,21 +352,21 @@ class CallHistory(CallHistoryBuilder):
     def get_indent(self):
         return self.stack.get_indent()
 
-    def call_enter(self, id, key, s_args, s_kwargs):
+    def call_enter(self, tick, key, s_args, s_kwargs):
         # TODO eliminate this indent thing -- use own stack or something
-        self.linear.append((self.get_indent(), 'enter', id, ))
-        self.calls[id] = [key, s_args, s_kwargs]
+        self.linear.append((self.get_indent(), 'enter', tick, ))
+        self.calls[tick] = [key, s_args, s_kwargs]
         try:
-            self.caller[id] = self.stack.top()[0]
+            self.caller[tick] = self.stack.top()[0]
         except TypeError:
-            self.caller[id] = None
+            self.caller[tick] = None
         self.reachability.updatePathTo(key, enumerate(reversed(self.stack)))
-        self.stack.append((id, key, s_args, s_kwargs, ))
+        self.stack.append((tick, key, s_args, s_kwargs, ))
 
-    def call_result(self, id, s_res, s_exc):
-        self.results[id] = [s_res, s_exc]
-        self.stack.popWhile(lambda item: item[0] != id)
-        self.linear.append((self.get_indent(), 'result', id, ))
+    def call_result(self, tick, s_res, s_exc):
+        self.results[tick] = [s_res, s_exc]
+        self.stack.popWhile(lambda item: item[0] != tick)
+        self.linear.append((self.get_indent(), 'result', tick, ))
 
     def get_object_history_until(self, callid, objid):
         for tupl in self.iterCalls():
@@ -376,11 +376,11 @@ class CallHistory(CallHistoryBuilder):
                 yield tupl
 
     def iterCalls(self, keyFilter=None):
-        for (id, (key, s_args, s_kwargs)) in self.calls.iteritems():
+        for (tick, (key, s_args, s_kwargs)) in self.calls.iteritems():
             if keyFilter and key != keyFilter:
                 continue
-            (s_res, s_exc) = self.results[id]
-            yield (id, key, s_args, s_kwargs, s_res, s_exc)
+            (s_res, s_exc) = self.results[tick]
+            yield (tick, key, s_args, s_kwargs, s_res, s_exc)
 
 
 class ObjectInfo:
@@ -409,7 +409,7 @@ class TestCodegen:
         for key in set([item[0] for item in self.callhistory.calls.itervalues()]):
             mock_func_name = self.gen_func_name("mock", key)
             code += "def " + mock_func_name + "(*args, **kwargs):\n"
-            for (id, key, s_args, s_kwargs, s_res, s_exc) in self.callhistory.iterCalls(keyFilter=key):
+            for (tick, key, s_args, s_kwargs, s_res, s_exc) in self.callhistory.iterCalls(keyFilter=key):
                 c_args = self.unserialize_code(s_args or self.marshal.empty_list())
                 c_kwargs = self.unserialize_code(s_kwargs or self.marshal.empty_dict())
                 c_ret = self.unserialize_code(s_res or self.marshal.none())
@@ -486,17 +486,15 @@ class TestCodegen:
         kk = key.split(".")
         return kk[2]
 
-    # TODO rename id to callid or timestamp everywhere
-
-    def c_replay_object(self, id, key):
+    def c_replay_object(self, tick, key):
         (object_name, class_name, module_name, consructor_args) = self.get_object_info(key)
         code = ""
         code += "    import " + module_name + "\n"
         code += "    " + object_name + " = " + module_name + "." + class_name + "(" + ( repr(consructor_args) if consructor_args else "") + ")\n"
-        if self.callhistory.object_calls.get(id):
-            objid = self.callhistory.object_calls[id]
-            code += "    # object id " + str(objid) + "\n"
-            for (old_id, old_key, old_s_args, old_s_kwargs, old_s_res, old_s_exc) in self.callhistory.get_object_history_until(id, objid):
+        if self.callhistory.object_calls.get(tick):
+            objid = self.callhistory.object_calls[tick]
+            code += "    # $" + str(objid) + "$\n"
+            for (old_id, old_key, old_s_args, old_s_kwargs, old_s_res, old_s_exc) in self.callhistory.get_object_history_until(tick, objid):
                 # TODO resolve old_s_*args recursively (for handling object arguments too)
                 if self.is_insideeffect(old_key, old_s_args, old_s_kwargs):
                     code += "    " + self.c_call_function(old_key, old_s_args, old_s_kwargs) + "\n"
@@ -514,20 +512,20 @@ class TestCodegen:
                 "import ent \n\n"
                 # TODO generate import code
         mockmap = {}
-        for (id, key, s_args, s_kwargs, s_res, s_exc) in self.callhistory.iterCalls():
-            if self.callhistory.isMockable(id):
+        for (tick, key, s_args, s_kwargs, s_res, s_exc) in self.callhistory.iterCalls():
+            if self.callhistory.isMockable(tick):
                 try:
-                    mockmap[self.callhistory.caller[id]].append(id)
+                    mockmap[self.callhistory.caller[tick]].append(tick)
                 except KeyError:
-                    mockmap[self.callhistory.caller[id]] = [id]
+                    mockmap[self.callhistory.caller[tick]] = [tick]
         code += self.mock_code(set())
         code += "class TestEnt(unittest.TestCase): \n\n" # TODO generate classname
-        for (id, key, s_args, s_kwargs, s_res, s_exc) in self.callhistory.iterCalls():
-            if not self.callhistory.isTestable(id):
+        for (tick, key, s_args, s_kwargs, s_res, s_exc) in self.callhistory.iterCalls():
+            if not self.callhistory.isTestable(tick):
                 continue
-            code += "  def " + self.gen_func_name("test", key, str(id)) + "(self):\n"
+            code += "  def " + self.gen_func_name("test", key, str(tick)) + "(self):\n"
             # Arrange
-            functions_to_mock = set([self.callhistory.calls[mockid][0] for mockid in mockmap[id]]) if id in mockmap else set()
+            functions_to_mock = set([self.callhistory.calls[mockid][0] for mockid in mockmap[tick]]) if tick in mockmap else set()
             code += self.mock_setup_code(functions_to_mock)
             if self.marshal.is_exception(s_exc):
                 code += "    try:\n"
@@ -540,7 +538,7 @@ class TestCodegen:
                 code += "      pass\n"
             else:
                 if self.is_object_method(key):
-                    code += self.c_replay_object(id, key)
+                    code += self.c_replay_object(tick, key)
                 # Act
                 code += "    actual = " + self.c_call_function(key, s_args, s_kwargs) + "\n"
                 # Assert
