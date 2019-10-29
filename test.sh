ce261547c8aab5a7c5808fba1aaba8ec48aaaaf3
Dávid Takács <takdavid@gmail.com>
2019-10-29 19:27:02 +0100

    Better test.

--- test.sh
+++ test.sh
@@ -14,6 +14,12 @@ mkdir -p importable_package/tests
 touch_py importable_package/tests/__init__.py
 touch_py importable_package/__init__.py
 
+mkdir -p not_a_package
+touch_py not_a_package/not_a_subpackage.py
+
+mkdir -p lambda-layers/my-lambda-layer/python
+touch_py lambda-layers/my-lambda-layer/python/my_lambda_module.py
+
 mkdir -p non-importable-dir/burried_package/burried_subpackage/
 touch_py non-importable-dir/burried_package/burried_subpackage/__init__.py
 touch_py non-importable-dir/burried_package/__init__.py
@@ -24,6 +30,7 @@ cat >> pytest.ini <<PYTESTINI;
 [pytest]
 python_paths =
     non-importable-dir
+    lambda-layers/my-lambda-layer/python
 PYTESTINI
 
 cat >.coveragerc <<COVERAGERC;
@@ -43,10 +50,18 @@ python ../boil.py
 
 function assert_f() { test -f $1 || (echo Missing $1; exit 1) }
 
+assert_f importable_package/tests/__init__.py
 assert_f importable_package/tests/test_subpackage.py
 assert_f importable_package/tests/test_importable_package.py
 
+assert_f not_a_package/__init__.py
+assert_f not_a_package/tests/__init__.py
+assert_f not_a_package/tests/test_not_a_subpackage.py
+
+assert_f lambda-layers/my-lambda-layer/tests/test_my_lambda_module.py
+
 assert_f non-importable-dir/burried_package/tests/test_burried_subpackage.py
+assert_f non-importable-dir/burried_package/tests/__init__.py
 assert_f non-importable-dir/tests/test_unreachable_script.py
 
 coverage run $(which pytest)
