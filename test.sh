ad6a37e349bf9a3230406588495f7242f0a9dd64
Dávid Takács <takdavid@gmail.com>
2019-10-29 19:14:50 +0100

    Better test.

--- test.sh
+++ test.sh
@@ -3,33 +3,36 @@ set -e
 
 # ARRANGE
 
+OLDDIR=$(pwd)
+mkdir testproject
+cd testproject
+
 function touch_py() { echo '1/1' > $1; }
-mkdir -p testproject/importable_package/subpackage
-touch_py testproject/importable_package/subpackage/__init__.py
-mkdir -p testproject/importable_package/tests
-touch_py testproject/importable_package/tests/__init__.py
-touch_py testproject/importable_package/__init__.py
+mkdir -p importable_package/subpackage
+touch_py importable_package/subpackage/__init__.py
+mkdir -p importable_package/tests
+touch_py importable_package/tests/__init__.py
+touch_py importable_package/__init__.py
 
-mkdir -p testproject/non-importable-dir/burried_package/burried_subpackage/tests
-touch_py testproject/non-importable-dir/burried_package/burried_subpackage/tests/__init__.py
-touch_py testproject/non-importable-dir/burried_package/burried_subpackage/__init__.py
-touch_py testproject/non-importable-dir/burried_package/__init__.py
-touch_py testproject/non-importable-dir/unreachable_script.py
+mkdir -p non-importable-dir/burried_package/burried_subpackage/
+touch_py non-importable-dir/burried_package/burried_subpackage/__init__.py
+touch_py non-importable-dir/burried_package/__init__.py
+touch_py non-importable-dir/unreachable_script.py
 
-cat >> testproject/pytest.ini <<PYTESTINI;
+# TODO this is only to fake the update of pytest.ini/PYTHONPATH
+cat >> pytest.ini <<PYTESTINI;
 [pytest]
 python_paths =
     non-importable-dir
 PYTESTINI
 
-cat >testproject/.coveragerc <<COVERAGERC;
+cat >.coveragerc <<COVERAGERC;
 [run]
 omit = $VIRTUAL_ENV/*
 COVERAGERC
 
 export COVERAGE_FILE=.coverage
 
-cd testproject
 coverage run $(which pytest) || true
 
 # ACT
@@ -38,18 +41,17 @@ python ../boil.py
 
 # ASSERT
 
-coverage run $(which pytest)
-
-cd ..
-set -e
 function assert_f() { test -f $1 || (echo Missing $1; exit 1) }
 
-assert_f testproject/importable_package/tests/subpackage/test_subpackage.py
-assert_f testproject/importable_package/tests/test_importable_package.py
+assert_f importable_package/tests/test_subpackage.py
+assert_f importable_package/tests/test_importable_package.py
 
-assert_f testproject/non-importable-dir/burried_package/burried_subpackage/tests/test_burried_subpackage.py
-assert_f testproject/non-importable-dir/test_unreachable_script.py
+assert_f non-importable-dir/burried_package/tests/test_burried_subpackage.py
+assert_f non-importable-dir/tests/test_unreachable_script.py
+
+coverage run $(which pytest)
 
 # CLEANUP
 
+cd $OLDDIR
 rm -rf testproject
