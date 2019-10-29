8054a66f8714d701763e3b20d00281ee20c4ed4e
Dávid Takács <takdavid@gmail.com>
2019-10-29 21:19:32 +0100

    Simplify computation. Better classes.

--- boil.py
+++ boil.py
@@ -1,17 +1,7 @@
 import inspect
 import os
-from pathlib import Path
 
 from coverage_helper import collect as coverage_collect
-
-PROJECT_ROOT = os.getcwd()  # FIXME ?
-
-
-def relative_filepath(fn):
-    if fn.startswith(PROJECT_ROOT):
-        return fn[len(PROJECT_ROOT):].lstrip('/\\')
-    else:
-        return fn
 
 
 def every_binary_split(head, tail=''):
@@ -22,69 +12,12 @@ def every_binary_split(head, tail=''):
         head = head2
 
 
-def bush(paths):
-    d = {}
-    for path in paths:
-        for head, tail in every_binary_split(path):
-            if head in d:
-                d[head].add(tail)
-            else:
-                d[head] = {tail,}
-    return d
-
-
-lambda_layer_roots = [str(p) for p in Path(PROJECT_ROOT).rglob('python')]
-lambda_layer_roots = [relative_filepath(p) for p in lambda_layer_roots if os.path.isdir(p)]
-lambda_function_files = [str(p) for p in Path(PROJECT_ROOT).rglob('lambda_function*')]
-lambda_function_roots = [relative_filepath(os.path.dirname(str(p))) for p in lambda_function_files if os.path.isfile(p)]
-init_files = [str(p) for p in Path(PROJECT_ROOT).rglob('__init__.py')]
-package_roots = [relative_filepath(os.path.dirname(str(p))) for p in init_files if os.path.isfile(p)]
-package_roots = set([p for p in package_roots if str(p)[0] != '.'])
-
-
 def suggest_path(package_name, suggested_dir, file_name):
     if file_name == '__init__.py':
         suggested_filename = 'test_' + package_name + '.py'
     else:
         suggested_filename = 'test_' + file_name
     return os.path.join(suggested_dir, suggested_filename)
-
-
-def find_package(relfn):
-    extend_syspath = []
-    touch_files = []
-    module_name = inspect.getmodulename(relfn)
-    if module_name == '__init__':
-        module_name = ''
-    package_dir = ''
-    package_name = module_name
-    for head, tail in every_binary_split(relfn):
-        if head in lambda_layer_roots:
-            package_dir = head
-            extend_syspath.append(package_dir)
-            break
-        if head in package_roots:
-            package_dir = head
-            package_name = os.path.basename(package_dir) + '.' + package_name
-            continue
-        break
-    package_name = package_name.strip('.')
-    if not package_dir:
-        print('Warning: source %s is not in a package' % relfn)
-        package_dir = os.path.dirname(relfn)
-        package_name = os.path.basename(package_dir)
-        if package_name.isidentifier():
-            touch_files.append(os.path.join(package_dir, '__init__.py'))
-    if not package_name.replace('.', '_').isidentifier():
-        if not module_name.isidentifier():
-            print('Warning: "%s" is impossible to import' % relfn)
-            package_name = ''
-        else:
-            print('Warning: importing %s as module %s' % (relfn, module_name))
-            package_name = module_name
-            extend_syspath.append(package_dir)
-
-    return package_dir, package_name, extend_syspath, touch_files
 
 
 class ProjectPrefences(object):
@@ -95,6 +28,34 @@ class ProjectPrefences(object):
         pass  # FIXME
 
 
+class Directory(object):
+
+    def __init__(self, path) -> None:
+        self.absolute = os.path.abspath(str(path))
+        self.relative = self.absolute[len(os.getcwd()):].lstrip('/\\')
+
+    def __str__(self) -> str:
+        return self.relative
+
+    def __add__(self, other):
+        return self.relative + str(other)
+
+    def is_lambda(self):
+        return os.path.basename(self.relative) == 'python'
+
+    def is_package_root(self):
+        return os.path.isfile(os.path.join(self.relative, '__init__.py'))
+
+    def __contains__(self, other):
+        return str(other.absolute).startswith(str(self.absolute))
+
+    def relative_filepath(self, fn):
+        a = os.path.abspath(str(fn))
+        b = self.absolute
+        if a.startswith(b):
+            return a[len(b):].lstrip('/\\')
+
+
 class Package(object):
     prefer = ProjectPrefences()
 
@@ -102,8 +63,7 @@ class Package(object):
         self.relfn = relfn
 
     def is_lambda(self):
-        return any((head in lambda_function_roots or head in lambda_layer_roots)
-                   for head, tail in every_binary_split(self.relfn))
+        return any((Directory(head).is_lambda()) for head, tail in every_binary_split(self.relfn))
 
     def has_test_subpackage(self):
         pass  # FIXME
@@ -116,21 +76,65 @@ class File(object):
     def is_test(self):
         return ('test' in inspect.getmodulename(self.relfn).split('_')) or ('tests' in self.relfn.split(os.sep))
 
+    def find_package(self):
+        pass
+
 
 class Project(object):
     prefer = ProjectPrefences()
+
+    root = Directory(os.getcwd())  # FIXME
+
+    def find_package(self, relfn):
+        extend_syspath = []
+        touch_files = []
+        module_name = inspect.getmodulename(relfn)
+        if module_name == '__init__':
+            module_name = ''
+        package_dir = ''
+        package_name = module_name
+        for head, tail in every_binary_split(relfn):
+            if Directory(head).is_lambda():
+                package_dir = head
+                extend_syspath.append(package_dir)
+                break
+            if Directory(head).is_package_root():
+                package_dir = head
+                package_name = os.path.basename(package_dir) + '.' + package_name
+                continue
+            break
+        package_name = package_name.strip('.')
+        if not package_dir:
+            print('Warning: source %s is not in a package' % relfn)
+            package_dir = os.path.dirname(relfn)
+            package_name = os.path.basename(package_dir)
+            if package_name.isidentifier():
+                touch_files.append(os.path.join(package_dir, '__init__.py'))
+        if not package_name.replace('.', '_').isidentifier():
+            if not module_name.isidentifier():
+                print('Warning: "%s" is impossible to import' % relfn)
+                package_name = ''
+            else:
+                print('Warning: importing %s as module %s' % (relfn, module_name))
+                package_name = module_name
+                extend_syspath.append(package_dir)
+
+        return package_dir, package_name, extend_syspath, touch_files
+
+
+
 
 
 def test_module_for_file(relfn):
     package = Package(relfn)
     project = Project()
     file_dir, file_name = os.path.split(relfn)
-    package_dir, package_name, _, _ = find_package(relfn)
+    package_dir, package_name, _, _ = project.find_package(relfn)
     test_file_base_name = '.'.join(package_name.split('.')[1:]) if '.' in package_name else package_name
 
-    project_test_dir = relative_filepath(os.path.join(PROJECT_ROOT, 'tests'))
+    project_test_dir = project.root + 'tests'
     inside_package_test_dir = os.path.join(package_dir, 'tests')
-    if PROJECT_ROOT < package_dir:
+    if Directory(package_dir) in project.root:
         outside_package_test_dir = os.path.join(os.path.dirname(package_dir), 'tests')
     else:
         outside_package_test_dir = project_test_dir
@@ -173,7 +177,8 @@ def touch(fn):
 
 
 for ana in coverage_collect():
-    relfn = relative_filepath(ana[0])
+    project = Project()
+    relfn = project.root.relative_filepath(ana[0])
     file = File(relfn)
     if file.is_test():
         continue
@@ -181,7 +186,7 @@ for ana in coverage_collect():
         testfn = test_module_for_file(relfn)
         print('%s %s for %s' % ('Updating' if os.path.isfile(testfn) else 'Creating', testfn, relfn))
 
-        package_dir, package_name, extend_syspath, touch_files = find_package(relfn)
+        package_dir, package_name, extend_syspath, touch_files = project.find_package(relfn)
 
         if package_name:
             test_dir = os.path.dirname(testfn)
